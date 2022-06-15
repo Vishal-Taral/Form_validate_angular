0206c44ab3b69e03bc21043b7549684056dc0fba
kalpeshingale14 <104881248+kalpeshingale14@users.noreply.github.com>
2022-06-15 09:43:56 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,32 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { UsdInrPipe } from './pipes/usd-inr.pipe';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    ForgetPasswordComponent
+    ForgetPasswordComponent,
+    UsdInrPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
+    MatToolbarModule,
+    NgbModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +10,16 @@ import { AuthService } from 'src/app/shared/auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  title='Kalpesh Ingale';
+
+  today=Date();
+
+  name='Splendid Infosystem';
+
+  user={
+    name:'kalpesh',
+    age:22
+  }
 
   constructor(private auth :AuthService) { }
 
@@ -17,5 +30,8 @@ export class DashboardComponent implements OnInit {
     this.auth.logout();
 
   }
+
+
+
 
 }
